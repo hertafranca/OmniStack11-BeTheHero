7f8fa58aaa1a29967f899ca42fa9641898bd5902
hertafranca <hertafranca.tech@gmail.com>
2020-03-25 02:58:04 -0300

    routes

--- backEnd/src/routes.js
+++ backEnd/src/routes.js
@@ -1,6 +1,9 @@
 import { Router } from "express";
 
 import OngController from "./controllers/OngController";
+import IncidentsController from "./controllers/IncidentsController";
+import ProfileController from "./controllers/ProfileController";
+import SessionController from "./controllers/SessionController";
 
 const routes = Router();
 
@@ -8,5 +11,13 @@ routes.get("/ongs", OngController.index);
 routes.post("/ongs", OngController.store);
 routes.put("/ongs/:id", OngController.update);
 routes.delete("/ongs/:id", OngController.delete);
+
+routes.get("/incidents", IncidentsController.index);
+routes.post("/incidents", IncidentsController.store);
+routes.delete("/incidents/:id", IncidentsController.delete);
+
+routes.get("/profile", ProfileController.index);
+
+routes.post("/session", SessionController.store);
 
 export default routes;
